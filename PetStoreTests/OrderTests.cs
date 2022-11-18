7e9defaa1355979cae8ae98659ddbe1bdf126f98
Joshua Mitchell <jmitchel021@student.wethinkcode.co.za>
2022-11-18 10:42:45 +0200

    Exercise 3 done

--- PetStoreTests/OrderTests.cs
+++ PetStoreTests/OrderTests.cs
@@ -8,11 +8,18 @@ namespace PetStoreTests;
 
 public class OrderTests
 {
+    readonly StoreApi _storeApi = new StoreApi(TestHelper.BasePath);
 
     [Test]
     [QaVerification.Grading]
     public void AddOrder()
     {
-        // Complete this test
+        Order order = new Order(id: 21);
+        
+        _storeApi.PlaceOrder(order);
+        
+        Assert.That(order, Is.EqualTo(_storeApi.GetOrderById(21)));
+        
+        _storeApi.DeleteOrder(21);
     }
 }
--- PetStoreTests/PetTests.cs
+++ PetStoreTests/PetTests.cs
@@ -7,35 +7,44 @@ using Applications.PetStore.Swagger.Model;
 using NUnit.Framework;
 
 namespace PetStoreTests;
-
 public class PetTests : TestHelper
 {
+    readonly PetApi _PetApi = new PetApi(TestHelper.BasePath);
+
+    private Pet Darth_Vader;
 
     [Test]
     [QaVerification.Grading]
     public void GetPetThatExists()
     {
-        // Complete this test
+        var pet = _PetApi.GetPetById(1);
+
+        Assert.That(pet!.Id, Is.EqualTo(1));
     }
 
     [Test]
     [QaVerification.Grading]
     public void GetPetThatDoesNotExist()
     {
-        // Complete this test
+        Assert.Throws<ApiException>(() => _PetApi.GetPetById(99));
     }
 
     [Test]
     [QaVerification.Grading]
     public void AddPet()
     {
-        // Complete this test
+        Darth_Vader = new Pet(id: 69, name: "Darth Vader", photoUrls: new List<string>(), tags: new List<Tag>());
+
+        _PetApi.AddPet(Darth_Vader);
+
+        Assert.That(Darth_Vader, Is.EqualTo(_PetApi.GetPetById(69)));
     }
 
     [Test]
     [QaVerification.Grading]
     public void RemovePet()
     {
-        // Complete this test
+        _PetApi.DeletePet(69);
+        Assert.Throws<ApiException>(() => _PetApi.GetPetById(69));
     }
 }
